4026bf0da5dc1bd2ebaecfb5ab9778eb60ca8028
FaithKamaraju <faithkamaraju@gmail.com>
2025-01-31 18:33:34 -0500

    sidepanel , cylinderMesh and SphereMesh added

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,32 @@
-import { createRoot } from "react-dom/client";
+import React, { useEffect, useRef } from "react";
+
 import { Canvas } from "@react-three/fiber";
 
-function App() {
-  return (
-    <Canvas>
-      <mesh>
-        <boxGeometry args={[2, 2, 2]} />
-        <meshPhongMaterial />
-      </mesh>
-      <ambientLight intensity={0.1} />
-      <directionalLight position={[0, 0, 5]} color="red" />
-    </Canvas>
-  );
+import Menu from "react-burger-menu/lib/menus/stack";
+
+import Cylinder from "./components/CylinderMesh/CylinderMesh.jsx";
+import Sphere from "./components/SphereMesh/SphereMesh.jsx";
+
+import "./App.css";
+import ControlPanel from "./components/ControlPanel/ControlPanel.jsx";
+
+class App extends React.Component {
+  showSettings(event) {
+    event.preventDefault();
+  }
+  render() {
+    return (
+      <>
+        <Menu width={"25%"} noOverlay noTransition itemListElement="div">
+          <h1>Random Shape and Form Generator</h1>
+          <ControlPanel />
+        </Menu>
+        <Canvas>
+          <pointLight position={[1, 1, 1]} />
+          <Sphere />
+        </Canvas>
+      </>
+    );
+  }
 }
 export default App;
